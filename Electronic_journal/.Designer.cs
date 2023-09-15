06a3dee7e5c95dbc03d146bde77f615d726080fc
fremov <59342062+fremov@users.noreply.github.com>
2023-09-15 21:55:15 +0300

    auth for teacher icon change fonts and sizes windows on center screen

--- Electronic_journal/.Designer.cs
+++ Electronic_journal/.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Electronic_journal
 {
-    partial class authorization1
+    partial class authorization
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(authorization));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(129, 229);
+            this.button1.Location = new System.Drawing.Point(97, 186);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 32);
+            this.button1.Size = new System.Drawing.Size(104, 66);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ученик";
             this.button1.UseVisualStyleBackColor = false;
@@ -50,42 +52,32 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Highlight;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(130, 180);
+            this.button2.Location = new System.Drawing.Point(97, 107);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 32);
+            this.button2.Size = new System.Drawing.Size(105, 65);
             this.button2.TabIndex = 1;
             this.button2.Text = "Учитель";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // authorization
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(346, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Выход";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // authorization1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(402, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(302, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "authorization1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "authorization";
             this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.authorization1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -93,7 +85,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ Electronic_journal/authorization_for_teacher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Electronic_journal
+{
+    public partial class authorization_for_teacher : Form
+    {
+        public authorization_for_teacher()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void authorization_for_teacher_Load(object sender, EventArgs e)
+        {
+            // Получаем размер экрана
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            // Получаем размер формы
+            int formWidth = this.Width;
+            int formHeight = this.Height;
+
+            // Вычисляем координаты для размещения формы посередине экрана
+            int left = (screenWidth - formWidth) / 2;
+            int top = (screenHeight - formHeight) / 2;
+
+            // Устанавливаем позицию формы
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(left, top);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (teacher_login.Text == "admin" || teacher_login.Text == "admin")
+            {
+               
+                this.Close();
+                profile_teatcher3 profile_teacher = new profile_teatcher3();      
+                profile_teacher.Show();
+
+            } else
+            {
+                MessageBox.Show("Проверьте введённые данные", "Ошибка");
+            }
+            
+        }
+    }
+}
